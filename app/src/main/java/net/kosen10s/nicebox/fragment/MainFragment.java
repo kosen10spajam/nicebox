c231e6160bcfa31042394ff3708920fe4e202e8d
SAKATA Sinji <contact@sinjis-view.mydns.jp>
2016-04-09 14:39:26 +0900

    StatusPreferenceを実装

--- app/src/main/java/net/kosen10s/nicebox/fragment/MainFragment.java
+++ app/src/main/java/net/kosen10s/nicebox/fragment/MainFragment.java
@@ -6,11 +6,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import net.kosen10s.nicebox.R;
 import net.kosen10s.nicebox.core.BaseFragment;
+import net.kosen10s.nicebox.preference.StatusPreference;
 
 /**
  * Created by e10dokup on 2016/04/09
@@ -21,15 +23,19 @@ public class MainFragment extends BaseFragment {
 
     @Bind(R.id.btn_nice)
     Button mNiceButton;
-    @Bind(R.id.btn_history)
-    Button mHistoryButton;
+    @Bind(R.id.text_status)
+    TextView mStatusText;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
+        StatusPreference preference = new StatusPreference(getBaseActivity());
+        int status = preference.getStatus();
 
         ButterKnife.bind(this, view);
+
+        mStatusText.setText(String.valueOf(status));
         return view;
     }
 }
