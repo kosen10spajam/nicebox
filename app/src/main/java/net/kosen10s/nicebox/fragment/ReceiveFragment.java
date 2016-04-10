28bef522e59c4fdf070f915e451283ea2a31ea30
Yoshihiro WADA <1365196t@stu.kobe-u.ac.jp>
2016-04-10 10:00:30 +0900

    Implement layout ReceiveFragment

--- app/src/main/java/net/kosen10s/nicebox/fragment/ReceiveFragment.java
+++ app/src/main/java/net/kosen10s/nicebox/fragment/ReceiveFragment.java
@@ -5,13 +5,16 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import net.kosen10s.nicebox.R;
 import net.kosen10s.nicebox.core.BaseFragment;
+import net.kosen10s.nicebox.preference.StatusPreference;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by yasuhiro on 4/9/16.
@@ -20,10 +23,10 @@ public class ReceiveFragment extends BaseFragment {
     private static final String TAG = ReceiveFragment.class.getSimpleName();
     private final ReceiveFragment self = this;
 
-    @Bind(R.id.text_nice)
-    TextView mTextNice;
-    @Bind(R.id.text_time)
-    TextView mTextTime;
+    @Bind(R.id.text_nice_point_value)
+    TextView mTextNicePoint;
+    @Bind(R.id.btn_back)
+    Button mBackButton;
 
     @Nullable
     @Override
@@ -31,6 +34,15 @@ public class ReceiveFragment extends BaseFragment {
         View view = inflater.inflate(R.layout.fragment_receive, container, false);
 
         ButterKnife.bind(this, view);
+
+        StatusPreference preference = new StatusPreference(getBaseActivity());
+        int status = preference.getStatus();
+        mTextNicePoint.setText(String.valueOf(status));
         return view;
+    }
+
+    @OnClick(R.id.btn_back)
+    public void onClickBackButton() {
+        popFragment();
     }
 }
